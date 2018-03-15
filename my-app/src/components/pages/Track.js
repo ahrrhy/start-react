610faec6c6078b41a798142bce18bc9a4250d675
ahrrhy <ahrrhy@gmail.com>
2018-03-15 18:49:03 +0200

    make tracks

--- my-app/src/components/pages/Track.js
+++ my-app/src/components/pages/Track.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
-const Track = ({ track }) => <div>{track.name}</div>;
+class Track extends Component {
 
-const mapStateToProps = (state, ownProps) => ({
-    track: state.tracks.find(track => track.id === Number(ownProps.track.id))
-});
-
-export default connect(mapStateToProps)(Track);
+    render() {
+        return (
+            <div>{this.props.track.name}</div>
+    );
+    }
+}
+export default connect(
+    (state, ownProps) => ({
+        track: state.tracks.find(track => track.id),
+        ownProps})
+)(Track);
